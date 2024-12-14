fd1cb8a665d617a784da3f9a665c6f20bd20a690
mohammedowais2003 <161530478+mohammedowais2003@users.noreply.github.com>
2024-12-14 21:03:16 +0500

    first commit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import { NavBar } from '../components/navbar';
 import { Footer } from '../components/footer';
 
 const ContactSection = () => {
-  // Form Submission Handler
+  
  
 
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { NavBar } from "./components/navbar";
 import { Hero } from "./components/hero";
 import { Footer } from "./components/footer";
-import Link from "next/link";
+
 
 
 
